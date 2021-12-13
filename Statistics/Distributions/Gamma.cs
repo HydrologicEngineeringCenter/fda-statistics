6f82ff0268fb1553b793d75750bf3c64dd6feab5
rnugent3 <richard.nugent.3@gmail.com>
2021-12-13 09:05:15 -0800

    inverse cdf

--- Statistics/Distributions/Gamma.cs
+++ Statistics/Distributions/Gamma.cs
@@ -62,7 +62,7 @@ namespace Statistics.Distributions
             if (x <= 0)
             {
                 return 0;
-            } else if(x >= double.MaxValue)
+            } else if(x >= double.PositiveInfinity)
             {
                 return 1;
             }
@@ -78,6 +78,33 @@ namespace Statistics.Distributions
         }
 
         public double InverseCDF(double p)
+        {
+            if (p <= 0.0)
+            {
+                return 0.0;
+            }
+            else if (p >= 1.0)
+            {
+                return double.PositiveInfinity;
+            }
+            else
+            {
+                double xMin = 0.0;
+                double xMax = 1.0;
+                for (int j = 0; j < 100; j++)
+                {
+                    double pMax = CDF(xMax);
+                    if (pMax > p)
+                    {
+                        return invCDFNewtonBiSearch(p, xMin, xMax, 1E-12, 100);
+                    }
+                    xMax *= 2.0;
+                }
+            }
+            
+        }
+
+        private double invCDFNewtonBiSearch(double p, double xMin, double xMax, double v1, int v2)
         {
             throw new NotImplementedException();
         }
