d583f6e07c26fb6e0280698f5b281e864ee0ce8e
rnugent3 <richard.nugent.3@gmail.com>
2021-12-13 16:38:58 -0800

    removed IDistribution

--- Statistics/Distributions/Gamma.cs
+++ Statistics/Distributions/Gamma.cs
@@ -8,40 +8,12 @@ using Utilities;
 
 namespace Statistics.Distributions
 {
-    class Gamma : IDistribution
+    class Gamma 
     {
         #region fields
         private double _Shape;
         private double _Scale;
         private const double DX = 1E-10;
-        #endregion
-        #region IDistribution Properties
-
-        public IDistributionEnum Type => throw new NotImplementedException();
-
-        public double Mean => throw new NotImplementedException();
-
-        public double Median => throw new NotImplementedException();
-
-        public double Variance => throw new NotImplementedException();
-
-        public double StandardDeviation => throw new NotImplementedException();
-
-        public double Min => throw new NotImplementedException();
-
-        public double Max => throw new NotImplementedException();
-
-        public double Skewness => throw new NotImplementedException();
-
-        public IRange<double> Range => throw new NotImplementedException();
-
-        public int SampleSize => throw new NotImplementedException();
-
-        public bool Truncated => throw new NotImplementedException();
-
-        public IMessageLevels State => throw new NotImplementedException();
-
-        public IEnumerable<IMessage> Messages => throw new NotImplementedException();
         #endregion
 
         #region Constructors
@@ -50,19 +22,10 @@ namespace Statistics.Distributions
             _Shape = shape;
             _Scale = scale;
         }
-
-        public void BuildFromProperties()
-        {
-            throw new NotImplementedException();
-        }
-        #endregion
-
-        #region Property Methods
-
         #endregion
 
 
-        #region IDistribution Methods
+        #region Methods
         public double CDF(double x)
         {
             if (x <= 0)
@@ -76,11 +39,6 @@ namespace Statistics.Distributions
             {
                 return SpecialFunctions.regIncompleteGamma(_Shape, x / _Scale);
             }
-        }
-
-        public bool Equals(IDistribution distribution)
-        {
-            throw new NotImplementedException();
         }
 
         public double InverseCDF(double p)
@@ -118,24 +76,10 @@ namespace Statistics.Distributions
                     _Shape * Math.Log(_Scale) - SpecialFunctions.logGamma(_Shape));
         }
 
-        public string Print(bool round = false)
-        {
-            throw new NotImplementedException();
-        }
-
-        public string Requirements(bool printNotes)
-        {
-            throw new NotImplementedException();
-        }
-
-        public XElement WriteToXML()
-        {
-            throw new NotImplementedException();
-        }
         #endregion
 
         #region Supporting Methods
-        protected  double invCDFNewtonBiSearch(double p, double xMin, double xMax, double tolX, int maxIter)
+        protected double invCDFNewtonBiSearch(double p, double xMin, double xMax, double tolX, int maxIter)
         {
             return newtonBiSearch(p, xMin, xMax, tolX, maxIter);
         }
