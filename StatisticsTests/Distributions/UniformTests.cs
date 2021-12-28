66fceddf4beb87d2992a5ef7d8900aa49bc21a05
Will Lehman <william.p.lehman@usace.army.mil>
2021-12-28 13:24:10 -0800

    added uniform tests fixes #35

--- StatisticsTests/Distributions/UniformTests.cs
+++ StatisticsTests/Distributions/UniformTests.cs
@@ -17,6 +17,24 @@ namespace StatisticsTests.Distributions
             Assert.Equal(min, testObj.Min, 2);
             Assert.Equal(max, testObj.Max, 2);
         }
-
+        [Theory]
+        [InlineData(0d, 1d, 0.5, 0.5)]
+        [InlineData(0d, 1d, 0.25, 0.25)]
+        [InlineData(0d, 1d, 0.75, 0.75)]
+        [InlineData(0d, 1d, 0.95, 0.95)]
+        [InlineData(1d, 2d, 0.5, 1.5)]
+        [InlineData(1d, 2d, 0.25, 1.25)]
+        [InlineData(1d, 2d, 0.75, 1.75)]
+        [InlineData(1d, 2d, 0.95, 1.95)]
+        [InlineData(1d, 3d, 0.5, 2)]
+        [InlineData(1d, 3d, 0.25, 1.5)]
+        [InlineData(1d, 3d, 0.75, 2.5)]
+        [InlineData(1d, 3d, 0.95, 2.90)]
+        public void Uniform_INVCDF(double min, double max, double prob, double expected)
+        {
+            var testObj = new Statistics.Distributions.Uniform(min, max);
+            double result = testObj.InverseCDF(prob);
+            Assert.Equal(result, expected, 5);
+        }
     }
 }
