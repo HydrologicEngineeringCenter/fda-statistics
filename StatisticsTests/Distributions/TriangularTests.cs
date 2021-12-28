a09885ef8c88b837fe665bd9f733fb1d3e366688
Will Lehman <william.p.lehman@usace.army.mil>
2021-12-28 13:28:38 -0800

    adding tests for triagular fixes #34

--- StatisticsTests/Distributions/TriangularTests.cs
+++ StatisticsTests/Distributions/TriangularTests.cs
@@ -21,5 +21,17 @@ namespace StatisticsTests.Distributions
         {
             Assert.Throws<InvalidConstructorArgumentsException>(() => new Statistics.Distributions.Triangular(1, 0, 0));
         }
+                [Theory]
+        [InlineData(0d,5d, 10d, 0.0, 0.0)]
+[InlineData(0d,5d, 10d, 0.75, 6.464466)]
+[InlineData(0d,5d, 10d, 0.25, 3.535534)]
+[InlineData(0d,5d, 10d, 0.5, 5.0)]
+[InlineData(0d,5d, 10d, 1.0, 10.0)]
+        public void Triangular_INVCDF(double min, double mostlikely, double max, double prob, double expected)
+        {
+            var testObj = new Statistics.Distributions.Triangular(min, mostlikely, max);
+            double result = testObj.InverseCDF(prob);
+            Assert.Equal(result, expected, 5);
+        }
     }
 }
