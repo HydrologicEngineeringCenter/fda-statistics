0385be50faff45980684b3f0bb221f653aaeba34
rnugent3 <richard.nugent.3@gmail.com>
2022-01-05 13:34:05 -0800

    added extrapolation method

--- Statistics/Graphical/Graphical.cs
+++ Statistics/Graphical/Graphical.cs
@@ -10,6 +10,9 @@ namespace Statistics.Distributions
     class Graphical
     {
         #region Fields
+        private double _pMax; //the maximum exceedance probability possible in the frequency curve
+        private double _pMin; //the minimum exceedanace probability possible in the frequency curve
+        private double _Tolerance = 0.0001;
         private int _SampleSize;
         private bool _Truncated;
         /// <summary>
@@ -23,7 +26,7 @@ namespace Statistics.Distributions
         /// <summary>
         /// _ExceedanceProbabilities represent the interpolated and extrapolated set of exceedance probabilities. this should probably always be the same.  
         /// </summary>
-        private double[] _ExceedanceProbabilities;
+        private double[] _RequiredExceedanceProbabilities = { .999, .99, .95, .90, .80, .70, .60, .50, .475, .45, .425, .40, .375, .35, .325, .30, .275, .25, .225, .20, .175, .15, .125, .10, .075, .05, .04, .025, .02, .015, .01, 0.009, 0.008, 0.007, 0.006, 0.005, 0.004, .002, .001 };
         /// <summary>
         /// _FlowOrStageDistributions represet the set of normal distributions with mean and standard deviation computed using the less simple method
         /// </summary>
@@ -62,11 +65,11 @@ namespace Statistics.Distributions
         {
             get
             {
-                return _ExceedanceProbabilities;
+                return _RequiredExceedanceProbabilities;
             }
             set
             {
-                _ExceedanceProbabilities = value;
+                _RequiredExceedanceProbabilities = value;
             }
         }
         [Stored(Name = "FlowOrStageDistributions", type = typeof(IDistribution[]))]
@@ -100,8 +103,8 @@ namespace Statistics.Distributions
 
         #region Constructor 
 
-        #endregion
-        public Graphical(double[] exceedanceProbabilities, double[] flowOrStageValues, int equivalentRecordLength, bool usingFlows = false, bool flowsAreLogged = false)
+      
+        public Graphical(double[] exceedanceProbabilities, double[] flowOrStageValues, int equivalentRecordLength, double maximumProbability, double minimumProbability, bool usingFlows = false, bool flowsAreNotLogged = false)
         {
             //1. Check for null data and check for monotonicity 
             if(exceedanceProbabilities != null && flowOrStageValues != null)
@@ -119,13 +122,16 @@ namespace Statistics.Distributions
             _SampleSize = equivalentRecordLength;
             _UsingFlows = usingFlows;
             _InputExceedanceProbabilities = exceedanceProbabilities;
+            _pMax = maximumProbability;
+            _pMin = minimumProbability;
 
             //2. Log flows if using flows 
             if (usingFlows)
             {
-                if (flowsAreLogged == true)
+                if (flowsAreNotLogged)
                 {
                     _InputFlowOrStageValues = LogFlows(flowOrStageValues);
+
                 } else
                 {
                     _InputFlowOrStageValues = flowOrStageValues;
@@ -135,9 +141,16 @@ namespace Statistics.Distributions
                 _InputFlowOrStageValues = flowOrStageValues;
             }
         }
+        #endregion
 
-        #region Private Methods
-        public bool IsMonotonicallyIncreasing(double[] arrayOfData)
+
+        #region Methods
+        public void ComputeGraphicalConfidenceLimits(bool useConstantStandardError, double probStdErrHighConst, double probStdErrLowConst)
+        {
+            ExtendFrequencyCurveBasedOnNormalProbabilityPaper();
+            //and then.....
+        }
+        private bool IsMonotonicallyIncreasing(double[] arrayOfData)
         {
 
             for (int i = 0; i < arrayOfData.Length - 1; i++)
@@ -149,7 +162,7 @@ namespace Statistics.Distributions
             }
             return true;
         }
-        public bool IsMonotonicallyDecreasing(double[] arrayOfData)
+        private bool IsMonotonicallyDecreasing(double[] arrayOfData)
         {
 
             for (int i = 0; i < arrayOfData.Length - 1; i++)
@@ -178,10 +191,65 @@ namespace Statistics.Distributions
             return loggedFlows;
         }
 
+        private void ExtendFrequencyCurveBasedOnNormalProbabilityPaper() //I think we need a better name. 
+        {
+            List<double> xvals = new List<double>();
+            List<double> yvals = new List<double>();
+            for (int i = 0; i < _InputExceedanceProbabilities.Count(); i++)
+            {
+                xvals.Add(_InputFlowOrStageValues[i]);
+                yvals.Add(_InputExceedanceProbabilities[i]);
+            }
 
-        #endregion
+            //more frequent of the frequency curve
+            if (_pMax - yvals.First() > _Tolerance)
+            { //if the maximum exceedance probability is sufficiently larger than the largest exceedance probabiltiy 
 
-        #region Public Methods
+
+                // let x1 be the lowest value in xvals 
+                double xl = xvals[0];
+
+                //insert the maximum probability into the first location 
+                yvals.Insert(0, _pMax);
+               
+                if (xl < 0) { xvals.Insert(0, 1.001 * xl); } //if the first value is negative then make it slightly more negative
+
+                if (xl > 0)
+                {
+                    xvals.Insert(0, .999 * xl);
+                } //insert a slightly smaller value 
+
+                else if (xl < -1.0e-4)
+                {
+                    xvals[0] = 1.001 * xl;//why are we doing it a second time?
+                }                   
+                else
+                {
+                    xvals.Insert(0, -1.0e-4);//so if xl is really close to zero, set the value equal to -1e-4?
+                } 
+            }
+            //less frequent end of the frequency curve
+            if (yvals.Last() - _pMin > _Tolerance)
+            {
+                Distributions.Normal standardNormalDistribution = new Distributions.Normal();
+                double probabilityLower = _pMin;
+                double p1 = yvals[yvals.Count - 2];
+                double p2 = yvals.Last();
+                double xk = standardNormalDistribution.InverseCDF(probabilityLower);
+                double xk1 = standardNormalDistribution.InverseCDF(p1);
+                double xk2 = standardNormalDistribution.InverseCDF(p2);
+                double x1 = xvals[xvals.Count - 2];
+                double x2 = xvals.Last();
+                double c = (xk2 - xk1) / (xk - xk1);
+                double upperFlowOrStage = ((x2 - x1) + c * x1) / c;
+                xvals.Add(upperFlowOrStage);
+                yvals.Add(probabilityLower);
+            }
+            _InputFlowOrStageValues = xvals.ToArray();
+            _InputExceedanceProbabilities = yvals.ToArray();
+        }
+        
+
         public bool Equals(IDistribution distribution)
         {
             throw new NotImplementedException();
