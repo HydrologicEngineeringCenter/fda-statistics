1b487d66bc7eec0245902bccb6746b2b252c8579
Will Lehman <william.p.lehman@usace.army.mil>
2022-01-10 08:33:37 -0800

    removing mathnet.numerics

--- Statistics/SampleStatistics.cs
+++ Statistics/SampleStatistics.cs
@@ -8,28 +8,128 @@ namespace Statistics
 {
     internal class SampleStatistics : ISampleStatistics
     {
-        private readonly MathNet.Numerics.Statistics.DescriptiveStatistics _Statistics;
+        private double _mean;
+        private double _sampleVariance;
+        private double _min;
+        private double _max;
+        private double _median;
+        private double _skew;
+        private double _kurtosis;
+        private Int64 _n;
 
-        public double Mean => _Statistics.Mean;
-        public double Median { get; }
-        public double Variance => _Statistics.Variance;
-        public double StandardDeviation => _Statistics.StandardDeviation;
-        public double Skewness => _Statistics.Skewness;
-        public double Kurtosis => _Statistics.Kurtosis;
+        public double Mean
+        {
+            get { return _mean; }
+        }
+        public double Median
+        {
+            get { return _median; }
+        }
+        public double Variance
+        {
+            get
+            {
+                return _sampleVariance * (double)((double)(_n - 1) / (double)_n);
+            }
+        }
+        public double StandardDeviation
+        {
+            get
+            {
+                return Math.Pow(Variance, 0.5);
+            }
+        }
+        public double Skewness
+        {
+            get { return _skew; }
+        }
+        public double Kurtosis
+        {
+            get { return _kurtosis; }
+        }
         public Utilities.IRange<double> Range { get; }
-        public int SampleSize => Utilities.ExtensionMethods.CastToInt(_Statistics.Count);
+        public int SampleSize
+        {
+            get { return (int)_n; }
+        }
         public IMessageLevels State { get; }
         public IEnumerable<IMessage> Messages { get; }
 
         internal SampleStatistics(IData data)
         {
-            _Statistics = new MathNet.Numerics.Statistics.DescriptiveStatistics(data.Elements);
-            Median = MathNet.Numerics.Statistics.SortedArrayStatistics.Median(data.Elements.ToArray());
-            Range = Utilities.IRangeFactory.Factory(_Statistics.Minimum, _Statistics.Maximum);
+            InitalizeStats(data.Elements);
+            Range = Utilities.IRangeFactory.Factory(_min, _max);
             State = Validate(new Validation.SummaryStatisticsValidator(), out IEnumerable<IMessage> msgs);
             Messages = msgs;
         }
+        internal void InitalizeStats(IOrderedEnumerable<double> observations)
+        {
+            foreach (double observation in observations)
+            {
+                if (_n == 0)
+                {
+                    _max = observation;
+                    _min = observation;
+                    _mean = observation;
+                    _sampleVariance = 0;
+                    _n = 1;
+                }
+                else
+                {
+                    if (observation > _max) _max = observation;
+                    if (observation < _min) _min = observation;
+                    _n += 1;
+                    double tmpMean = _mean + ((observation - _mean) / (double)_n);
+                    _sampleVariance = ((((double)(_n - 2) / (double)(_n - 1)) * _sampleVariance) + (Math.Pow(observation - _mean, 2)) / (double)_n);
+                    _mean = tmpMean;
+                }
+            }
 
+            double s = System.Math.Pow(_sampleVariance * (double)((double)(_n - 1) / (double)_n), 0.5);
+            double SkewSums = 0;
+            double ksums = 0;
+            double midpoint = ((double)_n - 1) / 2;
+            bool noRounding = false;
+            if (System.Math.Floor(midpoint) == midpoint)
+            {
+                noRounding = true;
+            }
+            Array.Sort(observations.ToArray());
+            int val = 0;
+            bool firstpass = true;
+            foreach (double observation in observations)
+            {
+                if (midpoint == val)
+                {
+                    if (noRounding)
+                    {
+                        _median = observation;
+                    }
+                    else
+                    {
+                        if (firstpass)
+                        {
+                            midpoint += 1;
+                            _median += observation;
+                            firstpass = false;
+                        }
+                        else
+                        {
+                            _median += observation;
+                            _median /= 2.0;
+                        }
+                    }
+
+                }
+                SkewSums = SkewSums + (System.Math.Pow((observation - _mean), 3.0));
+                ksums = ksums + System.Math.Pow(((observation - _mean) / s), 4.0);
+                val++;
+            }
+            double nd = (double)_n;
+            ksums = ksums * (nd * (nd + 1.0)) / ((nd - 1.0) * (nd - 2.0) * (nd - 3.0));
+            _skew = ((nd) * SkewSums) / ((nd - 1.0) * (nd - 2.0) * (System.Math.Pow(s, 3.0)));
+            _kurtosis = (ksums) - ((3.0 * (System.Math.Pow((nd - 1.0), 2.0))) / ((nd - 2.0) * (nd - 3.0)));
+        }
         public IMessageLevels Validate(Utilities.IValidator<ISampleStatistics> validator, out IEnumerable<IMessage> msgs)
         {
             return validator.IsValid(this, out msgs);
