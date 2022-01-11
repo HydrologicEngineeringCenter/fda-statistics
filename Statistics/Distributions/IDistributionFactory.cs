bc28b044e88d8c4e94f93d7ed51c9944379ce64a
Will Lehman <william.p.lehman@usace.army.mil>
2022-01-11 08:16:41 -0800

    removing unused imports that were hanging around

--- Statistics/Distributions/IDistributionFactory.cs
+++ Statistics/Distributions/IDistributionFactory.cs
@@ -3,7 +3,6 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Xml.Linq;
-using MathNet.Numerics.Statistics;
 using Statistics.Histograms;
 using Utilities;
 
