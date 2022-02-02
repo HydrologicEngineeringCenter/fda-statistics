a1504c845d44a463a88b75fe471a92bc4d363ebe
Brennan Beam <64556723+Brennan1994@users.noreply.github.com>
2022-02-02 11:51:54 -0800

    add test. fails

--- StatisticsTests/GraphicalRelationships/GraphicalTests.cs
+++ StatisticsTests/GraphicalRelationships/GraphicalTests.cs
@@ -67,7 +67,7 @@ namespace StatisticsTests.GraphicalRelationships
             }
         }
         [Theory]
-        [InlineData(new double[] { .99,.5,.1,.02,.01,.002 }, new double[] { 500,2000,34900,66900,86000,146000 }, 10, true, false)] //Based on Elkhorn River at Highway 91 Dodge County FIS 2008
+        [InlineData(new double[] { .99,.5,.1,.02,.01,.002 }, new double[] { 500,2000,34900,66900,86000,146000 }, 5, true, false)] //Based on Elkhorn River at Highway 91 Dodge County FIS 2008
         [InlineData(new double[] { .99, .95, .90, .85, .8, .75, .7, .65, .6, .55, .5, .45, .4, .35, .3, .25, .2, .15, .1, .05, .02, .01, .005, .0025 },
             new double[] { 6.6, 7.4, 8.55, 9.95, 11.5, 12.7, 13.85, 14.7, 15.8, 16.7, 17.5, 18.25, 19, 19.7, 20.3, 21.1, 21.95, 23, 24.2, 25.7, 27.4, 28.4, 29.1, 29.4 },
             20, false, false)]
@@ -86,5 +86,19 @@ namespace StatisticsTests.GraphicalRelationships
                 Assert.Contains(value, means);
             }
         }
+        [Theory]
+        [InlineData(new double[] { .99, .5, .1, .02, .01, .002 }, new double[] { 500, 2000, 34900, 66900, 86000, 146000 }, 5, true, false)] //Based on Elkhorn River at Highway 91 Dodge County FIS 2008
+        [InlineData(new double[] { .99, .95, .90, .85, .8, .75, .7, .65, .6, .55, .5, .45, .4, .35, .3, .25, .2, .15, .1, .05, .02, .01, .005, .0025 },
+            new double[] { 6.6, 7.4, 8.55, 9.95, 11.5, 12.7, 13.85, 14.7, 15.8, 16.7, 17.5, 18.25, 19, 19.7, 20.3, 21.1, 21.95, 23, 24.2, 25.7, 27.4, 28.4, 29.1, 29.4 },
+            20, false, false)]
+        public void ReturnSameNumberOfProbabilitesAsDistributions(double[] probs, double[] flows, int erl, bool usingFlows, bool flowsNotLogged)
+        {
+            Graphical graphical = new Graphical(probs, flows, erl, usingFlows: usingFlows, flowsAreNotLogged: flowsNotLogged);
+            graphical.ComputeGraphicalConfidenceLimits();
+            Normal[] dists = graphical.FlowOrStageDistributions;
+            double[] prob = graphical.ExceedanceProbabilities;
+            Assert.Equal(dists.Length, prob.Length);
+        }
+
     }
 }
